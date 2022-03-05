059bd54a02ed54641b33939dc8c0b9281f35c9ce
RoyaleHighrblx <96347364+RoyaleHighrblx@users.noreply.github.com>
2022-03-05 12:23:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+array_1 = ['pen','paper','bottle','book'];
+random_no = Math.floor((Math.random()*array_1.length)+1);
+Element_of_array = array_1[random_no];
+time_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
